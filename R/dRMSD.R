feb5bdfde51714223431852a8b41478115888a9a
hadovou <h.davoudkhani@iclould.com>
2021-07-09 18:49:26 +0100

    add a test uint to evaluate dRMSD function using the use_testthat to generate test directory
    ##TODO the example is still remain

--- R/dRMSD.R
+++ R/dRMSD.R
@@ -27,5 +27,5 @@
 #' @examples
 #' not defined yet
 dRMSD <- function(VEC1, VEC2, N_atm){
-  return(sqrt(sum((VEC1 - VEC2)**2)/(N_atm*(N_atm-1)))*10)
+  return(sqrt(sum((VEC1 - VEC2)**2)/(N_atm*(N_atm-1))))
 }
