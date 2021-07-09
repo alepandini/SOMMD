a5e9f7ea87e0441c9dc801f9a4a259abf4e6bfc7
hadovou <h.davoudkhani@iclould.com>
2021-07-09 16:39:11 +0100

    the Header has been added

--- R/dRMSD.R
+++ R/dRMSD.R
@@ -1,3 +1,31 @@
+#' distance root-mean-square deviation
+#' @author Hamid Davoukhani \email{h.davoudkhani@@gmail.com}
+#'
+#'
+#'
+#' To calculate the root-mean-square deviation between
+#' the atoms in two configurations
+#'
+#'
+#'\deqn{dRMSD = \sqrt{ 1/N(N-1) * \sum{(X_i - X_j)^2L}
+#'
+#'     where i != j}}
+#' X_i coorodinate of Atom i
+#'
+#'
+#' X_j coorodinate of Atom j
+#'
+#' N number of atoms
+#'
+#' @param VEC1   the reference vector
+#' @param VEC2  the second Conformation vector
+#' @param N_atm the number of atom
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' not defined yet
 dRMSD <- function(VEC1, VEC2, N_atm){
   return(sqrt(sum((VEC1 - VEC2)**2)/(N_atm*(N_atm-1)))*10)
 }
