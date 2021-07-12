221b0b0c5b50aeeb1f04200ae0cabaf5cc38fd6a
Shaziya2020 <shaziyaismail.m@gmail.com>
2021-07-12 13:05:24 +0100

    Changes made
    calculations updated

--- R/Calc_Dist_Mat.R
+++ R/Calc_Dist_Mat.R
@@ -9,5 +9,5 @@
 Calc_Dist_Mat <- function(VEC){
   N_atm <- (length(VEC)-1)/3
   MAT <- as.matrix((dist(t(matrix(VEC[2:length(VEC)], ncol=N_atm)), method='euclidean', upper=TRUE, diag=TRUE)))
-  return(VEC)
+  return(MAT)
 }
--- tests/testthat/test-Calc_Dist_Mat.R
+++ tests/testthat/test-Calc_Dist_Mat.R
@@ -1,7 +1,14 @@
 test_that("Calculating the Distance Matrix", {
-  a <- dm.VEC(pdbs$xyz[1,])
-  b <- dm.VEC(pdbs$xyz[2,])
+  VEC <- as.vector (1,2,3,4,5,6,7,8,9,10)
   N_atm <- (length(VEC)-1)/3
-  MAT <- as.matrix((dist(t(matrix(VEC[2:length(VEC)], ncol=N_atm)), method='euclidean', upper=TRUE, diag=TRUE)))
-  return(VEC)
+  mat <- matrix(VEC[2:length(VEC)], ncol=N_atm)
+  transposed_mat <- t(mat)
+  dist_mat <- dist(transposed_mat, method='euclidean', upper=TRUE, diag=TRUE)
+
+  expect_equal(Calc_dist_mat(VEC), dist_mat)
 })
+
+
+
+
+
