e393aca91c83766c2b972223d17324a76d5d747f
hadovou <h.davoudkhani@icloud.com>
2021-09-17 14:11:13 +0100

    change the normalize_matrix function with scaler which is completed

--- R/normalize_matrix.R
+++ R/normalize_matrix.R
@@ -1,32 +1,92 @@
 
-#' Normalizer the Distance or similar matrices
+#' Scaling the Distance or similar matrices by standardization or normalization
 #'
+#' @param method accepts a character string determines what method implemented
+#'   *minmax* = transform the range to 0 and
+#'   "robust" = ?
+#'   "maxabs" = transform to a range of [-1 , 1],
+#'   "standard" = normalize the distribution with zero-mean and unit-variance
+#' @param by
+#' _feature_ scale each feature of matrices independently
+#' _trajaxis_ scale each axis of 3d space independently
+#' _whole_ applying scaling method to the whole matrix
 #' @param mat a given  numerical matrix
-#' @param option the method of normalization iso or dimension-wise
 #'
 #' @return a normalized matrices with values between 0 and 1
 #' @export
 #'
 #' @examples
 #' not provided here yet
-normalize_matrix <- function(mat, option = "iso") {
+scaler <- function(mat, method, by = "feature") {
 
-  # if ( typeof(mat) != double) {
-  #   stop("the mat argument is not a numerical type, please assure providing a correct matrix")
-  # }
-  # if ( dim(mat)[1] <= 1 || dim(mat)[2] < 3) {
-  #   warning("the provided martix is not presenting a coordinates of series of atoms in 3d space")
-  # }
-
-  if ("iso" ==  option) {
-    iso_max <- max(mat)
-
-    iso_min <- min(mat)
-
-    normalMat <- (function(x, mi, ma) ((x-mi)/(ma - mi))) (mat, iso_min, iso_max)
-    #normalMat <- lapply(mat , f(function(x, iso_min, iso_max) ((x-iso_min)/(iso_max - iso_min))))
-  }
-return (normalMat)
+  output <- switch (by,
+    "feature" = feature(mat, method),
+    "whole" = scale(mat , method),
+    "trajaxis" = axis(mat , method)
+  )
+  return(output)
 }
+
+feature <- function (mat , method) {
+
+  output <-  apply(mat, 2, function(x) scale(x, method))
+  return(output)
+}
+
+axis <- function( mat , method){
+    ncols <- ncol(mat)
+    x_ind <- seq(1, ncols, 3)
+    y_ind <- seq(2, ncols, 3)
+    z_ind <- seq(3, ncols, 3)
+
+    axis_x <- mat[,x_ind]
+    axis_y <- mat[,y_ind]
+    axis_z <- mat[,z_ind]
+
+    scaled_x <- scale (axis_x, method)
+    scaled_y <- scale (axis_y, method)
+    scaled_z <- scale (axis_z, method)
+
+    output[,x_ind] <- scaled_x
+    output[,y_ind] <- scaled_y
+    output[,z_ind] <- scaled_z
+}
+
+
+scale <- function(mat , method){
+  switch (method,
+    "minmax" = minmax(mat),
+    "robust" = robust(mat),
+    "maxabs" = maxabs(mat),
+    "standard" = standard(mat)
+
+    )
+  }
+
+
+
+minmax <- function(mat){
+  min_dat <- min(mat)
+  max_dat <- max(mat)
+  minmax_output <- (mat - min_dat) / (max_dat - min_dat)
+
+  return(minmax_output)
+}
+
+standard <- function(mat) {
+  mean_dat <- mean(mat)
+  std_dat <- st(mat)
+  standard_output <-  (mat - mean_dat) / st
+  return (standard_output)
+}
+
+maxabs <- function(mat) {
+  abs_maximum <- max(abs(mat))
+  maxabs_output <- mat / abs_maximum
+  return(maxabs_output)
+}
+
+
+
 
 
