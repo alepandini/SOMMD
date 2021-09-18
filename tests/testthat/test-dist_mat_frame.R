d7cf6abafcde3309bdd37c6a8f89b99fe47c4334
Shaziya2020 <72149004+Shaziya2020@users.noreply.github.com>
2021-09-18 10:17:45 +0100

    Test to Calculate the Distance Matrix
    Test to  Calculate the Distance Matrix

---
+++ tests/testthat/test-dist_mat_frame.R
@@ -0,0 +1,10 @@
+test_that("Calculating Distance Matrix ", {
+  pdb <- bio3d::read.pdb( "4q21" )
+  k <- bio3d::dm(pdb,inds="calpha")
+  #filled.contour(k, nlevels = 10)
+  expect_equal(dist_mat_frame(pdb, inds = "calpha"),k)
+  if(bio3d::check.utility("muscle")){
+
+  pdbs <- bio3d::pdbaln( bio3d::get.pdb( c( "4q21", "521p"), path = tempdir() ), outfile = tempfile() )
+
+    }})
