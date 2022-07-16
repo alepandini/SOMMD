4f5177ebbf10c6db6e46b95fe3b6329965f26c55
Alessandro Pandini <alessandro.pandini@gmail.com>
2022-07-16 19:16:14 +0100

    fix Rdoc generation for rio functions

--- R/rio_xtc.R
+++ R/rio_xtc.R
@@ -1,18 +1,24 @@
+#' @useDynLib SOMMD, .registration=TRUE
+#' @export
+
 rio_read_xtc_natoms <- function(xtc_filename){
   natms <- .Call("rio_read_xtc_natoms_", xtc_filename)
   return(natms)
 }
 
+#' @export
 rio_read_xtc_nframes <- function(xtc_filename){
   nframes <- .Call("rio_read_xtc_nframes_", xtc_filename)
   return(nframes)
 }
 
+#' @export
 rio_read_xtc <- function(xtc_filename){
   xyz_3D_array <- .Call("rio_read_xtc_", xtc_filename)
   return(xyz_3D_array)
 }
 
+#' @export
 rio_coord_reshape <- function(xyz_3D_array){
   array_dim <- dim(xyz_3D_array)
   perm_xyz_array <- aperm(xyz_3D_array, c(2,1,3))
@@ -21,6 +27,7 @@ rio_coord_reshape <- function(xyz_3D_array){
   return(reshaped_xyz_array)
 }
 
+#' @export
 rio_read_xtc2xyz <- function(xtc_filename){
   xyz_3D_array <- rio_read_xtc(xtc_filename)
   reshaped_xyz_array <- rio_coord_reshape(xyz_3D_array)
