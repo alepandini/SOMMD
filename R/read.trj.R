40e850bca6d1b0f54486d5ec4e319b9e8bd77007
Alessandro Pandini <alessandro.pandini@gmail.com>
2022-07-18 18:18:44 +0100

    add empty trj output for read trj function

--- R/read.trj.R
+++ R/read.trj.R
@@ -1,4 +1,18 @@
-read.trj <- function(trj_filename){
-  fileExtension <- tools::file_ext(trj_filename)
-  return(fileExtension)
+#' @export
+read.trj <- function(trjfile, topfile){
+  trjfileExtension <- tools::file_ext(trjfile)
+  topfileExtension <- tools::file_ext(trjfile)
+
+  trj <- NULL
+  trj$trjfile <- trjfile
+  trj$trjformat <- trjfileExtension
+  trj$topformat <- topfileExtension
+  trj$topfile <- topfile
+  trj$coord <- matrix(0)
+  trj$top <- data.frame(0)
+  trj$start <- c(0)
+  trj$end<- c(0)
+  trj$call <- ""
+
+  return()
 }
