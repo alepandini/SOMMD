3b2d48adcb307129aef85f8c40eec6bdd3346d95
Stefano <stefano.motta@unimib.it>
2024-06-25 13:12:31 +0200

    Add test-function for struct2pdb

--- tests/testthat/test-read.gro.r
+++ tests/testthat/test-read.gro.r
@@ -1,27 +1,25 @@
 test_that("read.gro correctly parses .gro file", {
-  # Specifica il percorso del file di esempio
+  # Read gro file
   gro_file <- system.file("extdata/", "HIF2a.gro", package = "SOMMD")
-  
-  # Leggi il file .gro
   gro <- read.gro(gro_file)
   
-  # Verifica che l'output sia di classe 'gro'
+  # Check the class of the output
   expect_s3_class(gro, "gro")
   
-  # Verifica che i componenti dell'output siano corretti
+  # Check that output are correct
   expect_true(is.data.frame(gro$atom))
   expect_true(is.matrix(gro$xyz))
   expect_true(is.numeric(gro$box))
   
-  # Verifica che le colonne del dataframe siano corrette
+  # Check colnames
   expected_colnames <- c("resno", "resid", "elety", "eleno", "x", "y", "z", "Vx", "Vy", "Vz")
   expect_equal(colnames(gro$atom), expected_colnames)
   
-  # Verifica che i dati siano correttamente formattati
+  # Check format of the data
   expect_true(all(sapply(gro$atom[, c("resno", "eleno", "x", "y", "z", "Vx", "Vy", "Vz")], is.numeric)))
   expect_true(all(sapply(gro$atom[, c("resid", "elety")], is.character)))
 
-  #Verify the coordinates of an atom in trajectory
+  # Check coordinates of an atom
   result <- as.character(gro$atom[100,c(1:7)])
   expected_result <- c(244, "PHE", "O", 100, 5.287, 7.315, 3.876)
   expect_equal(result, expected_result, tolerance = 1e-6)
