68734e137deb4fff5b6b8b0440e9eed69e6b4fd8
Stefano <stefano.motta@unimib.it>
2024-06-25 10:09:47 +0200

    Add test-function for cluster.pathways

--- tests/testthat/test-cluster.pathways.r
+++ tests/testthat/test-cluster.pathways.r
@@ -19,3 +19,25 @@ test_that("cluster.pathways works correctly in time independent mode", {
   expect_equal(result$height, expected_height, tolerance = 1e-6)
 
 })
+
+# Funzione di test per cluster.pathways
+test_that("cluster.pathways works correctly in time dependent mode", {
+  # Caricare il SOM
+  som_file <- system.file("extdata", "SOM.rds", package = "SOMMD")
+  som_model <- readRDS(som_file)
+  # Definire le traiettorie
+  start <- c(1, 51, 101)
+  end <- c(50, 100, 150)
+  
+  # Eseguire la funzione
+  result <- cluster.pathways(SOM = som_model, start = start, end = end, time.dep="dependent")
+  
+  #Verify the obtained merge
+  expected_merge <- matrix(c(-2, -1, -3, 1), nrow=2)
+  expect_equal(result$merge, expected_merge, tolerance = 1e-6)
+
+  #Verify the obtained height
+  expected_height <- c(2.351409, 3.905692)
+  expect_equal(result$height, expected_height, tolerance = 1e-6)
+
+})
